c4db33f92b66c4558b988f2516d68751f324ac9f
priya1006pathak-dev <priya1006pathak@gmail.com>
2026-09-09 21:36:32 +0530

    Complete C++ Operators chapter

---
+++ 03_operators/prectice.cpp
@@ -0,0 +1,72 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b = 1;
+//     a = 10;
+//     if(++a){
+//     cout << b ;
+//      } else {
+//   cout << ++b;
+//     }
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 1;
+//     int b = 2;
+    
+//     if(--a > 0 && ++b > 2){
+
+//     cout << "Stage1 - Inside If " ;
+//      } else {
+//   cout << "Stage2 - Inside else  ";
+//     }
+//     cout << a << " "<< b << endl; 
+// }
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 1;
+//     int b = 2;
+    
+//     if(--a > 0 || ++b > 2){
+
+//     cout << "Stage1 - Inside If " ;
+//      } else {
+//   cout << "Stage2 - Inside else  ";
+//     }
+//     cout << a << " "<< b << endl; 
+// }
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int number = 3;
+//     cout << (25*(++number));
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 1;
+//     int b = a++;
+//     int c = ++a;
+//     cout << b ;
+//     cout << c;
+// }
